5102410e7750a23e38c841b34902fd961360b07e
Brian Yu <brian@brianyu.me>
2023-01-18 22:52:57 -0500

    Initial Commit

---
+++ Streamline/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Streamline
+//
+//  Created by Brian Yu on 1/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Streamline/StreamlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamlineApp.swift
+//  Streamline
+//
+//  Created by Brian Yu on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct StreamlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
